d54e7e566f1555e78115b60e250fa6ff1f0c4418
Adam Leskis <leskis@gmail.com>
2022-10-16 11:30:47 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Primm demo lesson 1
+
+Open this in Gitpod and run the activity!
---
+++ test.py
@@ -0,0 +1,7 @@
+name = "Billy"
+print("We want to know if you like programming!)
+print()
+print("Do you like programming " + Name + "?")
+answer = input(
+Print("Great! You said " + anwer + "!")
+print("Let's learn som Python today!")
